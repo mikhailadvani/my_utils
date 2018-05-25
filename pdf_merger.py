b8a695f496f52d1f093ef2219093cfc5a5f93026
Mikhail Advani <mikhail.advani@gmail.com>
2018-05-25 23:57:33 +0200

    Support for reverse

--- pdf_merger.py
+++ pdf_merger.py
@@ -4,12 +4,12 @@ import argparse
 import os
 from PyPDF2 import PdfFileMerger
 
-def get_input_pdfs(pdf_folder, pdf_files):
+def get_input_pdfs(pdf_folder, pdf_files, reverse):
     if pdf_files is not None:
         return pdf_files
     elif pdf_folder is not None:
         all_files = os.listdir(pdf_folder)
-        pdf_files = sorted(filter(lambda file: file.lower().endswith(".pdf"), all_files))
+        pdf_files = sorted(filter(lambda file: file.lower().endswith(".pdf"), all_files), reverse=reverse)
         return map(lambda file: os.path.join(pdf_folder, file), pdf_files)
     else:
         print "Need one of input-pdfs or folder"
@@ -20,9 +20,10 @@ def parse_args():
     arg_parser.add_argument('-o','--output-pdf', type=str, help='Output PDFs', required=True)
     arg_parser.add_argument('-i','--input-pdfs', nargs='+', help='Input PDFs')
     arg_parser.add_argument('-f','--folder',type=str,help='Input PDFs folder')
+    arg_parser.add_argument("-r", "--reverse", help="Reverse sort", action="store_true")
 
     args = arg_parser.parse_args()
-    return get_input_pdfs(args.folder, args.input_pdfs), args.output_pdf
+    return get_input_pdfs(args.folder, args.input_pdfs, args.reverse), args.output_pdf
 
 def merge(input_pdfs, output_pdf):
     merger = PdfFileMerger()
