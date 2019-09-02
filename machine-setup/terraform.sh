cf90ee5750e081b2d5c4fd986593e6871df6d042
Mikhail Advani <mikhail.advani@gmail.com>
2019-09-02 11:49:34 +0200

    Terraform setup fix

--- machine-setup/terraform.sh
+++ machine-setup/terraform.sh
@@ -6,6 +6,6 @@ TERRAFORM_TMP_DIR="/tmp/terraform"
 mkdir -p $TERRAFORM_TMP_DIR
 curl -o $TERRAFORM_TMP_DIR/terraform.zip $TERRAFORM_DOWNLOAD_URL
 unzip $TERRAFORM_TMP_DIR/terraform.zip -d $TERRAFORM_TMP_DIR
-mv $TERRAFORM_TMP_DIR/terraform /usr/local/bin/
-chmod +x /usr/local/bin/terraform
+sudo mv $TERRAFORM_TMP_DIR/terraform /usr/local/bin/
+sudo chmod +x /usr/local/bin/terraform
 rm -rf $TERRAFORM_TMP_DIR
